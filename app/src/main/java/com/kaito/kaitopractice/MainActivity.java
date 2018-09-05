0fd0f84bc1da9ae78a206ebb71b9ead22203c0bf
furukawakaito <k.furukawa.rkt@gmail.com>
2018-09-05 18:56:45 +0900

    課題２まで提出

--- app/src/main/java/com/kaito/kaitopractice/MainActivity.java
+++ app/src/main/java/com/kaito/kaitopractice/MainActivity.java
@@ -2,6 +2,10 @@ package com.kaito.kaitopractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.Date;
+import java.text.*;
+import android.widget.TextView;
+import java.text.DateFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 現在日時の取得
+        Date now = new Date(System.currentTimeMillis());
+
+        // 日時のフォーマットオブジェクト作成
+        DateFormat formatter = new SimpleDateFormat("yyyy年MM月dd日 HH時mm分");
+
+        // フォーマット
+        String nowText = formatter.format(now);
+
+        // 表示
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText(nowText);
     }
+
 }
